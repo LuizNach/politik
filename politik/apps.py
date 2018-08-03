94d651e5df9150400513338110ec376b4da40c0c
Hernando Silva <hernando.sas@gmail.com>
2018-08-02 23:40:37 -0300

    First commit

---
+++ politik/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PolitikConfig(AppConfig):
+    name = 'politik'
---
+++ politik/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Politician(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birthDate = models.DateField(null=True, blank=True)
+    office = models.TextField(max_length=50, blank=True)
+
+
+class LawProject(models.Model):
+    owner = models.OneToOneField(Politician, on_delete=models.DO_NOTHING)
+    description = models.TextField(max_length=500, blank=True)
+    passed = models.NullBooleanField(null=True)
+    createdAt = models.DateField(null=True, blank=True)
+    passedAt = models.DateField(null=True, blank=True)
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    law = models.ForeignKey(LawProject, on_delete=models.CASCADE)
+    vote = models.NullBooleanField(null=True)
