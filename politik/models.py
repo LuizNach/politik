a523edcaec09812319100a050a2d2d1429f79e02
Hernando Silva <hernando.sas@gmail.com>
2018-08-03 00:12:34 -0300

    Add first api endpoints
    Signed-off-by: Hernando Silva <hernando.sas@gmail.com>

--- politik/models.py
+++ politik/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Politician(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
--- politik_project/urls.py
+++ politik_project/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
+from rest_framework import routers
+from politik import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
