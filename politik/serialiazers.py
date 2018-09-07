44710a10b5cbba1a12ba6054a8449f150a980421
Chan Kyung Kim <ckyungk@gmail.com>
2018-09-07 15:22:37 -0300

    create CRUD

--- politik/serialiazers.py
+++ politik/serialiazers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from politik.models import Politician, LawProject
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,13 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+class PoliticianSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Politician
+        fields = ('id', 'user_id', 'bio', 'location', 'office')
+
+class LawSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = LawProject
+        fields = ('id', 'description', 'passed')
--- politik/views.py
+++ politik/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from .serialiazers import UserSerializer, GroupSerializer
+from rest_framework import viewsets, response
+from .serialiazers import UserSerializer, GroupSerializer, PoliticianSerializer, LawSerializer
+from politik.models import Politician, LawProject
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,6 +15,19 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
+class UserViewDetails(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.filter(username="admin")
+    serializer_class = UserSerializer
+
+class PoliticianViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Politician.objects.all().order_by('id')
+    serializer_class = PoliticianSerializer
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
@@ -17,3 +35,10 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class LawsViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = LawProject.objects.all().order_by('id')
+    serializer_class = LawSerializer
